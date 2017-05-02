1d466498114cf576059d3a38293f0f2bd4bc6641
Andrew Bartimote <s3582954@student.rmit.edu.au>
2017-05-02 19:27:52 +1000

    Modified code for creating game grids

--- src/application/GameGrid.java
+++ src/application/GameGrid.java
@@ -3,37 +3,44 @@ package application;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 
+
+
+/*My GameGrid class has 3 variable:
+  - buttonSize is an integer used to set the size of each grid button
+  - startX is the starting X coordinate of the Grid
+  - startY is the starting Y coordinate of the Grid
+*/
 public class GameGrid {
 
-	private int boardSize;
 	private int buttonSize;
 	private int startX;
 	private int startY;
-	public boolean hasShip;
 	
-	public GameGrid (int boardSize, int buttonSize, int startX, int startY){
-		this.boardSize = boardSize;
+	public GameGrid (int buttonSize, int startX, int startY){
 		this.buttonSize = buttonSize;
 		this.startX = startX;
 		this.startY = startY;
 	}
-	
-	public Node createGrid() {
-		Button[][] button = new Button[boardSize][boardSize];
-		Group grid = new Group();
+		
+/*This method takes a 2D array of buttons then creates a new GridPane
+  and place them buttons on the GridPane which is then returned.
+ */
+	public Node createGrid(Button[][] button) {
+		GridPane grid = new GridPane(); 						
 		for (int y=0; y < button.length; y++) {
         	for (int x=0; x < button.length; x++) {
         		button[x][y] = new Button();
         		button[x][y].setPrefSize(buttonSize,buttonSize);
         		button[x][y].setLayoutX(x * buttonSize + startX);
         		button[x][y].setLayoutY(y * buttonSize + startY);
-        		button[x][y].setText(".");
-        	    grid.getChildren().add(button[x][y]);
+        	    grid.add(button[x][y], x, y);
+        	    grid.setLayoutX(startX);
+        	    grid.setLayoutY(startY);
         	}
         }
 		return grid; 		
 	}
-
-	
 }
+
--- src/application/Play.java
+++ src/application/Play.java
@@ -1,6 +1,7 @@
 package application;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.text.Font;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -10,9 +11,23 @@ import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Play extends Application {
+	
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	//Basic code for taking a shot 
+	//I have Hard Coded some ships and attempted shot at the bottom of this file
+	public void takeShot(Button shot) {
+		if(shot.getText() == "SH") {
+			shot.setText("X");
+			shot.setFont(Font.font("Ariel", 16));
+		}
+		else {
+			shot.setText("O");
+			shot.setFont(Font.font("Ariel", 16));
+		}
 	}
 
     @Override
@@ -22,16 +37,55 @@ public class Play extends Application {
         //Creates a new root node
         Group root = new Group();
         
-        GameGrid player = new GameGrid(10,40,150,200);
-        root.getChildren().add(player.createGrid());
+        //creates a 2D array of buttons for the player and computer grid
+        Button[][] player = new Button[10][10];
+        Button[][] computer = new Button[10][10];
         
-        GameGrid computer = new GameGrid(10,40,650,200);
-        root.getChildren().add(computer.createGrid());
         
-        primaryStage.setScene(new Scene(root, 1200, 800));
-        primaryStage.show();
+        //new GameGrid created for the player
+        GameGrid playerGrid = new GameGrid(40,190,160);
+        //createGrid method used to put playerGrid on root node
+        root.getChildren().add(playerGrid.createGrid(player));
+        
+        //new GameGrid created for the computer
+        GameGrid computerGrid = new GameGrid(40,690,160);
+        //createGrid method used to put computerGrid on root node
+        root.getChildren().add(computerGrid.createGrid(computer));
+        
+        
+        //following code used to Hard Code ships on player grid
+        player[1][1].setText("SH");
+        player[1][2].setText("SH");
+        player[1][3].setText("SH");
+        player[1][4].setText("SH");
+        player[1][5].setText("SH");
+        
+        player[4][1].setText("SH");
+        player[5][1].setText("SH");
+        player[6][1].setText("SH");
+        player[7][1].setText("SH");
+        
+        player[4][8].setText("SH");
+        player[5][8].setText("SH");
+        player[6][8].setText("SH");
+
+        player[4][3].setText("SH");
+        player[4][4].setText("SH");
+        player[4][5].setText("SH");
+        
+        player[8][5].setText("SH");
+        player[8][6].setText("SH");
+        
+        //Hard Coded shot on the player board
+        takeShot(player[3][3]);
+        takeShot(player[4][4]);
+        takeShot(player[5][5]);
+        takeShot(player[6][6]);
+        takeShot(player[7][7]);
+
+        primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.show();      
         
     } 
-    
-                 
+                     
 }
