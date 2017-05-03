5908c878be3a5b2b73853d302989d75cbb3b6795
mpbeach <s3464768@student.rmit.edu.au>
2017-05-03 22:19:22 +1000

    mpbeach: boardview update

--- src/application/Board.java
+++ src/application/Board.java
@@ -5,25 +5,24 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
+import java.io.*;
 
 public class Board extends Application {
 	
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("BoardView.fxml"));
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("board.css").toExternalForm());
-			primaryStage.setTitle("Naval Assault");
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("BoardView.fxml"));
+		//BoardViewController controller = (BoardViewController)loader.getController();
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("board.css").toExternalForm());
+		primaryStage.setTitle("Naval Assault");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		primaryStage.setResizable(false);
 	}
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	
 }
