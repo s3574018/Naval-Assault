1af8fb7b88b79c3ffc9c99ac9eb2942f5ae8972b
mpbeach <s3464768@student.rmit.edu.au>
2017-05-09 22:48:12 +1000

    mpbeach: Created grids in FXML then populated in the BoardController, cleanup on MainApp and removed Grid.java from bits.navalassault.model as it is now covered inside of the controller.

--- src/bits/navalassault/view/BoardOverviewController.java
+++ src/bits/navalassault/view/BoardOverviewController.java
@@ -2,10 +2,18 @@ package bits.navalassault.view;
 
 import bits.navalassault.MainApp;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 public class BoardOverviewController {
+	
+	@FXML 
+	private GridPane playerGrid;
+	
+	@FXML
+	private GridPane computerGrid;
 	
     @FXML
     private Label statusLabel;
@@ -20,10 +28,32 @@ public class BoardOverviewController {
     }
     	
     @FXML
-    private void initialize() {
+    private void initialize() { 
+    	showPlayerGrid();
+    	showComputerGrid();
 	}
     
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
+    }
+    
+    public void showPlayerGrid() {
+    	for (int y=0; y < 10; y++) {
+	       	for (int x=0; x < 10; x++) {
+	       		Button button = new Button();
+	       		button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+	       		playerGrid.add(button, x, y);
+	       	}
+    	}
+    }
+    
+    public void showComputerGrid() {
+    	for (int y=0; y < 10; y++) {
+	       	for (int x=0; x < 10; x++) {
+	       		Button button = new Button();
+	       		button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+	       		computerGrid.add(button, x, y);
+	       	}
+    	}
     }
 }
