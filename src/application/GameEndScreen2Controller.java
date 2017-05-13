64efd6d8e779c2ba815f19b07a38ee58ef372522
nickira <nickira1228@gmail.com>
2017-05-14 00:11:48 +1000

    youLostFxml

---
+++ src/application/GameEndScreen2Controller.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+public class GameEndScreen2Controller {
+	@FXML
+	private Button btnYes;
+	@FXML
+	private Button btnNo;
+
+}
