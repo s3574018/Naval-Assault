5918c76465d06c3216a9e14f61ce63853f9d44d9
nickira <nickira1228@gmail.com>
2017-05-13 23:49:51 +1000

    EndScreen

---
+++ src/application/GameEndScreenController.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+public class GameEndScreenController {
+	@FXML
+	private Button btnYes;
+	@FXML
+	private Button btnNo;
+
+}
